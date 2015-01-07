79a78b72106fad50030a8b6b2da9e76e5c05fb1c
bossvn <nnbinh1987@outlook.com>
2015-01-07 17:04:52 +0700

    - modified

--- _scripts/export.py
+++ _scripts/export.py
@@ -12,17 +12,72 @@ from odf.style import Style, TableColumnProperties
 from odf.table import Table, TableRow, TableColumn, TableCell, CoveredTableCell
 from odf.text import P
 
+ods_result = load("../texts/text_simple.ods")
 
+ods_android = load("../texts/text_simple.ods")
+book_android = []
+
+
+def parse_ods(file):
+    book = []
+    # for each sheet
+    for table in file.spreadsheet.getElementsByType(Table):
+        name = table.getAttribute('name')
+        data = dict()
+        # for each row
+        for r, row in enumerate(table.getElementsByType(TableRow)):
+            c = 0
+            # for each cell
+            for cell in row.getElementsByType(TableCell):
+                # Check if there some merged cells
+                repeat = cell.getAttribute("numbercolumnsrepeated")
+                if not repeat:
+                    repeat = 1
+
+                # Load cell text
+                text = unicode("")
+                for p in cell.getElementsByType(P):
+                    # print p.encode(sys.stdout.encoding, errors='replace')
+                    # p.setAttribute('text',"aaa")
+                    # print p.childNodes
+                    if not len(p.attributes):
+                        for t in p.childNodes:
+                             p.removeChild(t)
+                    assert isinstance(p.childNodes, object)
+                    print p.childNodes
+
+                    p.addText("abcss")
+
+                    if not len(p.attributes):
+                        for t in p.childNodes:
+                            text += unicode(t)
+                            print unicode(t).encode(sys.stdout.encoding, errors='replace')
+
+                # Applied text for merged cell
+                for rr in range(int(repeat)):
+                    if text:
+                        data[r, c] = text
+                        print ' Sheet : ' + name.encode(sys.stdout.encoding,
+                                                        errors='replace') + '  Row %d , Cell %d   :  ' % (
+                            r, c) + text.encode(sys.stdout.encoding, errors='replace')
+                    c += 1
+        book.append((name, data))
+    return book
+
+# --------------------------------------------------------------------------------------
 def make_ods():
 
-    result_file = load("../texts/text.ods")
-
-    android_file = load("../texts/text _android.ods")
-
     # Loop to count all sheet (table)
-    for table in result_file.spreadsheet.getElementsByType(Table):
+    for table in ods_result.spreadsheet.getElementsByType(Table):
+        # table.setAttribute('name',"AAA")
         name = table.getAttribute('name')
         print name
+
+        for r, row in enumerate(table.getElementsByType(TableRow)):
+            c = 0
+            # for cell in row.getElementsByType(TableCell):
+                # print cell.getAttribute('text')
+
 
 
 
@@ -57,7 +112,8 @@ def make_ods():
             tr.addElement(tc)
     '''
 
-    # assert isinstance(ods.save, object)
-    # ods.save("text-merged-result.ods")
+    assert isinstance(ods_result.save, object)
+    ods_result.save("text-merged-result.ods")
 
+book_android = parse_ods(ods_android)
 make_ods()
